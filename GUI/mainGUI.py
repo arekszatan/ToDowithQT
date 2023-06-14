32fc9a3c063cc2f0383a584b7d648ca56291b93e
Arkadiusz Szatan <arek.szatan@interia.pl>
2023-06-14 12:56:22 +0200

    Add json file for data, and more

--- GUI/mainGUI.py
+++ GUI/mainGUI.py
@@ -1,9 +1,12 @@
 from PySide6 import QtWidgets
-from PySide6.QtWidgets import QPushButton
+from PySide6.QtWidgets import (QPushButton, QLabel, QCheckBox, QSpacerItem, QSizePolicy, QWidget, QGridLayout)
+from PySide6.QtGui import QPixmap
+from PySide6.QtCore import QSize, Qt, Signal, QObject, Slot
 from GUI.mainView import Ui_MainWindow
 import qasync
 import sys
 import logging as log
+import json
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -12,8 +15,34 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.setWindowTitle("ToDo application")
         self.stackedWidget.setCurrentIndex(0)
+        self.image.setText("<img src='GUI/img/zdjecieToDo.jpg' height='700' width='1000'/>")
         self.startButton.clicked.connect(self.start_button_clicked)
         self.addButton.clicked.connect(self.add_button_clicked)
+        self.id_num = 0
+        self.data = {}
+        self.open_json_file()
+
+    def open_json_file(self):
+        try:
+            f = open("data.json", "r", encoding="utf-8")
+            self.data = json.load(f)
+            f.close()
+        except FileNotFoundError:
+            log.exception("Cant open data file")
+            return
+        self.toDoName.setText(self.data["name"])
+        for i in self.data['toDoList']:
+            self.id_num = int(i['id'])
+            item = ToDoItemsWidgets(self.id_num, i['toDo'], i['isChecked'])
+            self.gridLayout_4.addWidget(item)
+
+    def change_json_file(self, new_data, filename='data.json'):
+        with open(filename, 'r+') as file:
+            file_data = json.load(file)
+            file_data["toDoList"].append(new_data)
+            file.seek(0)
+            json.dump(file_data, file, indent=4)
+        self.data = file_data
 
     def start_button_clicked(self):
         log.info("Button start_button_clicked was clicked")
@@ -21,6 +50,18 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def add_button_clicked(self):
         log.info("Button add_button_clicked was clicked")
+        new_text = self.newToDoLineEdit.text()
+        if not new_text:
+            return
+        self.id_num += 1
+        item = ToDoItemsWidgets(self.id_num, new_text)
+        self.gridLayout_4.addWidget(item)
+        new_to_do = {"id": str(self.id_num),
+                     "toDo": new_text,
+                     "isChecked": str(0)
+                     }
+        self.change_json_file(new_to_do)
+        self.newToDoLineEdit.clear()
 
 
 def start_application():  # Start Application with qasync
@@ -31,3 +72,69 @@ def start_application():  # Start Application with qasync
     log.info("Start front to application")
     with loop:
         loop.run_forever()
+
+
+class ToDoItemsWidgets(QWidget, QObject):
+
+    def __init__(self, id_int=0, new_text="", is_checked=0, parent=None):
+        super(ToDoItemsWidgets, self).__init__(parent)
+        log.info(f'Object QWidget was creating with id {str(id_int)}')
+        self.i = id_int
+        self.main_layout = QGridLayout()
+        self.setLayout(self.main_layout)
+        title = QLabel(str(self.i))
+        title.setFixedWidth(60)
+        title.setAlignment(Qt.AlignCenter)
+        self.main_layout.addWidget(title, 0, 0)
+        self.text_to_do = QLabel(new_text)
+        self.main_layout.addWidget(self.text_to_do, 0, 1)
+        check_box = QCheckBox("Gotowe")
+        check_box.clicked.connect(self.check_box_checked)
+        check_box.setFixedWidth(100)
+        check_box.setChecked(True) if is_checked == "1" else check_box.setChecked(False)
+        self.setStyleSheet("QLabel{color:gray}") if is_checked == "1" else self.setStyleSheet("QLabel{color:white}")
+        self.main_layout.addWidget(check_box, 0, 2)
+        close_button = QPushButton("Usuń")
+        close_button.setFixedWidth(100)
+        close_button.clicked.connect(self._close_widget)
+        self.main_layout.addWidget(close_button, 0, 3)
+
+    def _close_widget(self):
+        log.info(f'Object QWidget was deleting with id {str(self.i)}')
+        self.remove_item(self.i)
+        self.deleteLater()
+
+    def check_box_checked(self, is_checked):
+        log.info(f'QCheckBox was clicked with id {str(self.i)}')
+
+        if is_checked:
+            self.setStyleSheet("QLabel{color:gray}")
+            self.update_check_state(self.i, is_checked=str(1))
+        else:
+            self.update_check_state(self.i, is_checked=str(0))
+            self.setStyleSheet("QLabel{color:white}")
+
+    def remove_item(self, index, filename='data.json'):
+        with open(filename, 'r') as file:
+            file_data = json.load(file)
+            for idx, obj in enumerate(file_data["toDoList"]):
+                if int(obj['id']) == index:
+                    file_data["toDoList"].pop(idx)
+
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(json.dumps(file_data, indent=2))
+
+    def update_check_state(self, index, filename='data.json', is_checked=str(0)):
+        with open(filename, 'r') as file:
+            file_data = json.load(file)
+            for idx, obj in enumerate(file_data["toDoList"]):
+                if int(obj['id']) == index:
+                    file_data["toDoList"].pop(idx)
+                    text = {"id": index,
+                            "toDo": self.text_to_do.text(),
+                            "isChecked": is_checked
+                            }
+                    file_data["toDoList"].insert(idx, text)
+
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(json.dumps(file_data, indent=2))
--- GUI/mainView.py
+++ GUI/mainView.py
@@ -15,8 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QLabel, QMainWindow,
-    QPushButton, QSizePolicy, QStackedWidget, QVBoxLayout,
+from PySide6.QtWidgets import (QAbstractScrollArea, QApplication, QGridLayout, QLabel,
+    QLineEdit, QMainWindow, QPushButton, QScrollArea,
+    QSizePolicy, QSpacerItem, QStackedWidget, QVBoxLayout,
     QWidget)
 
 class Ui_MainWindow(object):
@@ -24,6 +25,23 @@ class Ui_MainWindow(object):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.resize(1200, 800)
+        MainWindow.setStyleSheet(u"QLabel{\n"
+"	\n"
+"}\n"
+"\n"
+"QWidget{\n"
+"background-color:#555;\n"
+"color:white;\n"
+"font-size:20px;\n"
+"}\n"
+"\n"
+"QPushButton{\n"
+"background-color:#121212;\n"
+"padding:10px;\n"
+"border:solid;\n"
+"border-radius: 2px;\n"
+"border-color:#555555;\n"
+"}")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.verticalLayout = QVBoxLayout(self.centralwidget)
@@ -34,15 +52,16 @@ class Ui_MainWindow(object):
         self.stackedWidget.setObjectName(u"stackedWidget")
         self.entryPage = QWidget()
         self.entryPage.setObjectName(u"entryPage")
-        self.entryPage.setAutoFillBackground(True)
+        self.entryPage.setAutoFillBackground(False)
         self.entryPage.setStyleSheet(u"")
         self.gridLayout = QGridLayout(self.entryPage)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.label = QLabel(self.entryPage)
-        self.label.setObjectName(u"label")
-        self.label.setScaledContents(True)
+        self.image = QLabel(self.entryPage)
+        self.image.setObjectName(u"image")
+        self.image.setScaledContents(True)
+        self.image.setAlignment(Qt.AlignCenter)
 
-        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.image, 0, 0, 1, 1)
 
         self.startButton = QPushButton(self.entryPage)
         self.startButton.setObjectName(u"startButton")
@@ -59,15 +78,59 @@ class Ui_MainWindow(object):
         self.mainToDo.setObjectName(u"mainToDo")
         self.gridLayout_2 = QGridLayout(self.mainToDo)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.scrollArea = QScrollArea(self.mainToDo)
+        self.scrollArea.setObjectName(u"scrollArea")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.scrollArea.sizePolicy().hasHeightForWidth())
+        self.scrollArea.setSizePolicy(sizePolicy1)
+        self.scrollArea.setSizeAdjustPolicy(QAbstractScrollArea.AdjustToContents)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollAreaWidgetContents = QWidget()
+        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 1180, 664))
+        self.gridLayout_3 = QGridLayout(self.scrollAreaWidgetContents)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.toDoWidget = QWidget(self.scrollAreaWidgetContents)
+        self.toDoWidget.setObjectName(u"toDoWidget")
+        self.gridLayout_4 = QGridLayout(self.toDoWidget)
+        self.gridLayout_4.setObjectName(u"gridLayout_4")
+
+        self.gridLayout_3.addWidget(self.toDoWidget, 0, 0, 1, 1)
+
+        self.spacerWidget = QWidget(self.scrollAreaWidgetContents)
+        self.spacerWidget.setObjectName(u"spacerWidget")
+        self.verticalLayout_2 = QVBoxLayout(self.spacerWidget)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalSpacer = QSpacerItem(20, 601, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout_2.addItem(self.verticalSpacer)
+
+
+        self.gridLayout_3.addWidget(self.spacerWidget, 1, 0, 1, 1)
+
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+
+        self.gridLayout_2.addWidget(self.scrollArea, 1, 0, 1, 2)
+
         self.addButton = QPushButton(self.mainToDo)
         self.addButton.setObjectName(u"addButton")
 
-        self.gridLayout_2.addWidget(self.addButton, 0, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.addButton, 3, 0, 1, 2)
 
-        self.removeButton = QPushButton(self.mainToDo)
-        self.removeButton.setObjectName(u"removeButton")
+        self.newToDoLineEdit = QLineEdit(self.mainToDo)
+        self.newToDoLineEdit.setObjectName(u"newToDoLineEdit")
 
-        self.gridLayout_2.addWidget(self.removeButton, 0, 1, 1, 1)
+        self.gridLayout_2.addWidget(self.newToDoLineEdit, 2, 0, 1, 2)
+
+        self.toDoName = QLabel(self.mainToDo)
+        self.toDoName.setObjectName(u"toDoName")
+        sizePolicy.setHeightForWidth(self.toDoName.sizePolicy().hasHeightForWidth())
+        self.toDoName.setSizePolicy(sizePolicy)
+        self.toDoName.setAlignment(Qt.AlignCenter)
+
+        self.gridLayout_2.addWidget(self.toDoName, 0, 0, 1, 2)
 
         self.stackedWidget.addWidget(self.mainToDo)
 
@@ -77,14 +140,17 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
+        self.stackedWidget.setCurrentIndex(1)
+
+
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"image", None))
+        self.image.setText("")
         self.startButton.setText(QCoreApplication.translate("MainWindow", u"Start", None))
-        self.addButton.setText(QCoreApplication.translate("MainWindow", u"dodaj", None))
-        self.removeButton.setText(QCoreApplication.translate("MainWindow", u"usun", None))
+        self.addButton.setText(QCoreApplication.translate("MainWindow", u"Dodaj", None))
+        self.toDoName.setText(QCoreApplication.translate("MainWindow", u"-", None))
     # retranslateUi
 
